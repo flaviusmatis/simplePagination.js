8612cda8adb17a6769a77bcad42defe60fa3e23a
Flavius Matis <flavius@fdbk.me>
2011-12-11 21:38:41 +0200

    Adding documentation.

---
+++ script.js
@@ -0,0 +1,19 @@
+DlHighlight.HELPERS.highlightByName("code", "pre");
+
+$('#light-pagination').pagination({
+	pages: 20,
+	cssStyle: 'light-theme',
+	displayedPages: 5
+});
+
+$('#dark-pagination').pagination({
+	pages: 120,
+	cssStyle: 'dark-theme',
+	displayedPages: 5
+});
+
+$('#compact-pagination').pagination({
+	pages: 120,
+	cssStyle: 'compact-theme',
+	displayedPages: 7
+});
