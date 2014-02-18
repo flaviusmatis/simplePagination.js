05868f5ce5af55da3b97382efc617a5fca865af1
Michael Misshore <michael@mudbugmedia.com>
2014-02-18 13:39:43 -0600

    Flesh out functionality with failing tests

--- tests/spec/SimplePaginationSpec.js
+++ tests/spec/SimplePaginationSpec.js
@@ -103,4 +103,46 @@ describe('SimplePagination', function() {
         })
     })
 
+    describe('invertPageOrder Option', function() {
+        var invertedPager;
+
+        beforeEach(function() {
+            $('<div id="inverted_pager" class="pager"></div>').appendTo('body').pagination({
+                items: items,
+                itemsOnPage: itemsOnPage,
+                invertPageOrder: true
+            });
+
+            invertedPager = $('#inverted_pager');
+        })
+
+        it('moves the highest page number to the front of the list', function() {
+            var expectedPage = pageCount;
+
+            expect(invertedPager).toBeOnPage(expectedPage);
+        })
+
+        describe('#nextPage', function() {
+            it('pages to the next page', function() {
+                var expectedPage = pager.pagination('getCurrentPage') - 1;
+
+                pager.pagination('nextPage');
+
+                expect(pager).toBeOnPage(expectedPage);
+            })
+        })
+
+        describe('#prevPage', function() {
+            it('pages to the previous page', function() {
+                pager.pagination('selectPage', 1);
+
+                var expectedPage = pager.pagination('getCurrentPage') + 1;
+
+                pager.pagination('prevPage');
+
+                expect(pager).toBeOnPage(expectedPage);
+            })
+        })
+    })
+
 });
